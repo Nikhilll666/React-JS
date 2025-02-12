fe1cb7be1172c1d349e108c41b9774706745b078
Nikhil666 <nikhil.15092005@gmail.com>
2025-02-12 21:43:36 +0530

    third-commit

---
+++ practiceSet_4/src/App.jsx
@@ -0,0 +1,47 @@
+// import React, { useRef } from "react";
+import React from "react";
+// import { useState } from "react";
+// import { useForm } from "react-hook-form";
+
+function App() {
+
+  // const name = useRef(null)
+  // const handleClick = (details) => {
+  //   details.preventDefault();
+  //   console.log(name.current.value);
+  // }
+
+  // const [val, setVal] = useState({name: "", email: ""})
+  // const handleClick = (event) => {
+  //   event.preventDefault();
+  //   console.log(val);
+  // }
+
+  // const { register, handleSubmit } = useForm()
+
+  return (
+      // <div>
+      //   <form action="" onSubmit={handleClick}>
+      //     <input ref = {name} className="border-2 m-5 p-1" type="text" placeholder="Enter your name"/>
+      //     <input className="px-3 py-1 bg-red-500 rounded-full text-white" type="submit"/>
+      //   </form>
+      // </div>
+
+      // <div>
+      //   <form action="" onSubmit={handleClick}>
+      //     <input onChange={(event) => setVal({...val, name: event.target.value})} type="text" placeholder="name" />
+      //     <input onChange={(event) => setVal({...val, email: event.target.value})} type="email" placeholder="email" />
+      //     <input type="submit" />
+      //   </form>
+      // </div>
+
+      // <div>
+      //   <form action="" onSubmit={handleSubmit((data) => console.log(data))}>
+      //     <input {...register('name')} type="text" placeholder="Name"/>
+      //     <input {...register('email')} type="email" placeholder="Email"/>
+      //     <input type="submit" />
+      //   </form>
+      // </div>
+    )
+}
+export default App;
